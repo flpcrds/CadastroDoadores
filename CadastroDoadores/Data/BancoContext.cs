736a35591b68241603a67f27a71c0f967a590f5a
Gerson Cardoso Filho <gersoncflho@mac.com>
2022-11-27 14:15:44 +0000

    fix entity framework configuration

--- CadastroDoadores/Data/BancoContext.cs
+++ CadastroDoadores/Data/BancoContext.cs
@@ -8,10 +8,31 @@ using System.Threading.Tasks;
 
 namespace CadastroDoadores.Data
 {
-    public class BancoContext : DbContext
+    public partial class BancoContext : DbContext
     {
+        public BancoContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost,1433;Database=DB_SistemaDoadores;User Id=sa;Password=MyPass@word;Trusted_Connection=False; ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);   
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
         public BancoContext(DbContextOptions<BancoContext> options) : base(options)
         {
+            
         }
 
         public DbSet<ContatoModel> Doadores { get; set; } 
